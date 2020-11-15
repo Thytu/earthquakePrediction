c7185de76f771d47e23293ba10e891bb7be15f5e
Thytu <valentin.de-matos@epitech.eu>
2020-11-15 12:43:50 +0100

    *utils: change sequ_size from 200 to 100

--- utils.py
+++ utils.py
@@ -3,7 +3,8 @@ import numpy as np
 
 from sklearn.utils import shuffle
 
-SEQU_SIZE = 200 # will be resize to 200
+SEQU_SIZE = 100
+MAX_DATA_SIZE = 1_000_000
 
 def load_dataset(path):
     dataset = []
@@ -12,7 +13,7 @@ def load_dataset(path):
         for index, line in enumerate(f):
             dataset.append(int(line.split(",")[0]))
             labels.append(float(line.split(",")[1]))
-            if index >= 4_000_000: # will be resize to 40M
+            if index >= MAX_DATA_SIZE:
                 break
 
     return dataset, labels
@@ -26,17 +27,15 @@ def create_seq(dataset, labels, seq_size):
     for index in range(len(dataset)):
         if len(data_batch) == SEQU_SIZE:
             dataset_result.append(data_batch)
-            # dataset_result.append(torch.Tensor(data_batch))
             labels_result.append(label_batch[-1])
             data_batch = []
             label_batch = []
         else:
-            data_batch.append(dataset[index])
+            data_batch.append([dataset[index]])
             label_batch.append(labels[index])
 
     dataset_result, labels_result = shuffle(dataset_result, labels_result)
-    return torch.tensor(dataset_result, dtype=torch.int16).unsqueeze(1), torch.tensor(labels_result, dtype=torch.float32)
-    # return dataset_result, torch.tensor(labels_result, dtype=torch.int64)
+    return torch.tensor(dataset_result, dtype=torch.int16).unsqueeze(2), torch.tensor(labels_result, dtype=torch.float32)
 
 def split(dataset, labels, test_ratio=0.2):
     X_train = dataset[:int(len(dataset) * (1-test_ratio))]
